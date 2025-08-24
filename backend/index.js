cf6000a50d7bd04eb38b9e7abc681aae399b898e
pritamgupta9 <pritamgupta9911@gmail.com>
2025-08-24 08:34:36 +0530

    added

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+// Import express
+const express = require('express');
+const path = require('path');
+
+// Create an express app
+const app = express();
+
+// Define a port
+const PORT = 8000;
+
+console.log(path.resolve(__dirname,"../reactcountryproject3/dist"));
+
+
+app.use(express.static(path.resolve(__dirname,"../reactcountryproject3/dist")))
+
+// Middleware to parse JSON (optional)
+app.use(express.json());
+
+// Simple route
+app.get('/api', (req, res) => {
+  res.send('Hello World! 🚀');
+});
+
+// Another route example
+app.get('api/about', (req, res) => {
+  res.send('This is a simple Express server.');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
